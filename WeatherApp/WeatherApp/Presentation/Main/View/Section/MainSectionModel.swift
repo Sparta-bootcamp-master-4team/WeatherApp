76c46a6b32c3a1d4d793d5652a239f82cdc7d703
ksyq12 <ksyq12@daum.net>
2025-05-26 17:00:50 +0900

    feat: 섹션 헤더 추가

--- WeatherApp/WeatherApp/Presentation/Main/View/Section/MainSectionModel.swift
+++ WeatherApp/WeatherApp/Presentation/Main/View/Section/MainSectionModel.swift
@@ -9,22 +9,22 @@ import Foundation
 import RxDataSources
 
 enum MainSectionModel {
-    case daily(items: [SectionItem])
-    case hourly(items: [SectionItem])
+    case daily(items: [SectionItem], header: String)
+    case hourly(items: [SectionItem], header: String)
 }
 
 extension MainSectionModel: SectionModelType {
     var items: [SectionItem] {
         switch self {
-        case .daily(let items): return items
-        case .hourly(let items): return items
+        case .daily(let items, _): return items
+        case .hourly(let items, _): return items
         }
     }
     
     init(original: MainSectionModel, items: [SectionItem]) {
         switch original {
-        case .daily: self = .daily(items: items)
-        case .hourly: self = .hourly(items: items)
+        case .daily(_, let header): self = .daily(items: items, header: header)
+        case .hourly(_, let header): self = .hourly(items: items, header: header)
         }
     }
     
--- WeatherApp/WeatherApp/Presentation/Main/View/SectionHeaderView.swift
+++ WeatherApp/WeatherApp/Presentation/Main/View/SectionHeaderView.swift
@@ -11,7 +11,7 @@ import SnapKit
 final class SectionHeaderView: UICollectionReusableView {
     static let id = "SectionHeaderView"
     
-    private let titleLabel: UILabel = {
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 14, weight: .bold)
         return label
